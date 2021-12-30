a6b1595daed409661e2cddb796def30a941e89d2
Andrei Krasnov <krasnyi5@rambler.ru>
2021-12-30 16:37:18 +0300

    Signed-off-by: Andrei Krasnov <krasnyi5@rambler.ru>

---
+++ .top.menu.php
@@ -0,0 +1,18 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Задание 1", 
+		"/task1/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Задание 2", 
+		"/task2/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
